be53ee31a107e9272f9d433f53e7bcd5cdbd6520
Oluwakorede Fashokun <koredefashokun@gmail.com>
2020-07-26 22:51:06 +0100

    Remove env.ts

--- src/env.ts
+++
@@ -1,8 +0,0 @@
-import Constants from 'expo-constants';
-
-// export const wsUri = `ws://${Constants.manifest.debuggerHost
-//   ?.split(":")
-//   ?.shift()
-//   ?.concat(":8536")}/api/v1/ws`;
-//
-export const wsUri = `wss://dev.lemmy.ml/api/v1/ws`;
