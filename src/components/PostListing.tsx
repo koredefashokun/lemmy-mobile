41d56200e38f993a7151b2b45914203e4f08e7ed
Oluwakorede Fashokun <koredefashokun@gmail.com>
2020-08-02 23:50:48 +0100

    Begin adding upvote/downvote functionality

--- src/components/PostListing.tsx
+++ src/components/PostListing.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
 import moment from 'moment';
-import { Post, CommunityUser, UserView, BanType } from '../interfaces';
+import {
+  Post,
+  CommunityUser,
+  UserView,
+  BanType,
+  CreatePostLikeForm
+} from '../interfaces';
 import { getMomentLanguage } from '../utils';
 import { colors } from '../styles/theme';
-import { useNavigation } from '@react-navigation/native';
+import useWebSocketService from '../hooks/useWebSocketService';
+import { SitesContext } from '../contexts/SitesContext';
 
 interface PostListingState {
   showEdit: boolean;
@@ -42,17 +50,17 @@ const PostListing: React.FC<PostListingProps> = (props) => {
   const initialState: PostListingState = {
     showEdit: false,
     showRemoveDialog: false,
-    removeReason: null,
+    removeReason: null as any,
     showBanDialog: false,
-    banReason: null,
-    banExpires: null,
+    banReason: null as any,
+    banExpires: null as any,
     banType: BanType.Community,
     showConfirmTransferSite: false,
     showConfirmTransferCommunity: false,
     imageExpanded: false,
     viewSource: false,
     showAdvanced: false,
-    my_vote: props.post.my_vote,
+    my_vote: props.post.my_vote as any,
     score: props.post.score,
     upvotes: props.post.upvotes,
     downvotes: props.post.downvotes
@@ -64,11 +72,83 @@ const PostListing: React.FC<PostListingProps> = (props) => {
     initialState
   );
 
+  const { activeSite, loading } = React.useContext(SitesContext);
+  const service = useWebSocketService({ activeSite, loading });
+
   React.useEffect(() => {
     const lang = getMomentLanguage();
     moment.locale(lang);
   }, []);
 
+  const handlePostLike = () => {
+    // TODO: Make sure that the user is authenticated first.
+    // Avoid mutating state (anti-pattern).
+
+    // if (!UserService.Instance.user) {
+    // this.context.router.history.push(`/login`);
+    // }
+
+    const new_vote = state.my_vote == 1 ? 0 : 1;
+
+    if (state.my_vote == 1) {
+      state.score--;
+      state.upvotes--;
+    } else if (state.my_vote == -1) {
+      state.downvotes--;
+      state.upvotes++;
+      state.score += 2;
+    } else {
+      state.upvotes++;
+      state.score++;
+    }
+
+    state.my_vote = new_vote;
+
+    let form: CreatePostLikeForm = {
+      post_id: props.post.id,
+      score: state.my_vote
+    };
+
+    service?.likePost(form);
+    setState(state);
+    // setupTippy();
+  };
+
+  const handlePostDislike = () => {
+    // TODO: Make sure the user is authenticated first.
+    // Avoid mutating state.
+
+    // if (!UserService.Instance.user) {
+    //   this.context.router.history.push(`/login`);
+    // }
+
+    const new_vote = state.my_vote == -1 ? 0 : -1;
+
+    if (state.my_vote == 1) {
+      state.score -= 2;
+      state.upvotes--;
+      state.downvotes++;
+    } else if (state.my_vote == -1) {
+      state.downvotes--;
+      state.score++;
+    } else {
+      state.downvotes++;
+      state.score--;
+    }
+
+    state.my_vote = new_vote;
+
+    let form: CreatePostLikeForm = {
+      post_id: props.post.id,
+      score: state.my_vote
+    };
+
+    service?.likePost(form);
+    setState(state);
+    // setupTippy();
+  };
+
+  // Remove wrapping TouchableOpacity to make upvote/downvote buttons clickable
   return (
     <TouchableOpacity
       onPress={() => navigate('Post', { postId: props.post.id })}
@@ -97,7 +177,10 @@ const PostListing: React.FC<PostListingProps> = (props) => {
         </Text>
         <View style={{ marginTop: 10 }}>
           <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <TouchableOpacity style={{ marginRight: 5 }}>
+            <TouchableOpacity
+              style={{ marginRight: 5 }}
+              onPress={handlePostLike}
+            >
               <Feather
                 name='arrow-up'
                 color={state.my_vote === 1 ? colors.info : '#999'}
@@ -107,7 +190,10 @@ const PostListing: React.FC<PostListingProps> = (props) => {
             <Text style={{ fontSize: 16, fontWeight: '500', color: '#999' }}>
               {state.score}
             </Text>
-            <TouchableOpacity style={{ marginLeft: 5 }}>
+            <TouchableOpacity
+              style={{ marginLeft: 5 }}
+              onPress={handlePostDislike}
+            >
               <Feather name='arrow-down' color='#999' size={20} />
             </TouchableOpacity>
           </View>
